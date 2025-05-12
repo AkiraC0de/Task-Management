0b8741f0f79060d62ffa1f4ca26eba84bae86ca5
Akir4aa <ravenakira22@gmail.com>
2025-05-12 20:49:40 +0800

    Set up: connection to the database

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const connectToDB = require('./db/connect')
 
 const app = express();
 
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-    console.log(`The Server is running at PORT ${process.env.PORT}`)
+    connectToDB();
+    console.log(`The Server is running at PORT ${process.env.PORT}`);
 })
