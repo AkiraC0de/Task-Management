affe1d0f2ac71b8c12c361ac6dfb40e31328d5ae
Akir4aa <ravenakira22@gmail.com>
2025-05-13 23:17:27 +0800

    feat(Task): added Task model

---
+++ models/Task.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    deadline: {
+        type: Date,
+        trim: true
+    },
+    status: {
+        type: String,
+        enum: ['in progress', 'completed', 'failed'],
+        require: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, { timestamps: true });
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
