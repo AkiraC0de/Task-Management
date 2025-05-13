f34b4b944bb73e56ee17fff466053763761f6673
Akir4aa <ravenakira22@gmail.com>
2025-05-13 23:06:41 +0800

    feat(User): added User model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+        select: false
+    },
+    role: {
+        type: String,
+        enum: ['user, admin'],
+        default: 'user'
+    },
+    profileImage: {
+        type: String, 
+        default: ''
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    }
+}, { 
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
