fe50372a2ef6b064bc20dcc6430150adf3a3282f
Akir4aa <ravenakira22@gmail.com>
2025-05-16 23:47:05 +0800

    feat(updateTask): added update task routes and its conrollers

--- backend/controllers/Task.controller.js
+++ backend/controllers/Task.controller.js
@@ -1,4 +1,4 @@
-const Task = require('../models/Task')
+const Task = require('../models/Task');
 
 const createTask = async (req, res) => {
     try {
@@ -27,4 +27,29 @@ const createTask = async (req, res) => {
     }
 } 
 
-module.exports = { createTask }
+const updateTask = async (req, res) => {
+    const user = req.user
+    const taskId = req.params.id;
+    const { title, description, deadline, status } = req.body;
+
+    // Validate the ID paramter if it has content
+    if(!taskId) res.status(400).json({success: false, message: 'Undefined Task ID parameter'});
+
+    const task = await Task.findById(taskId);
+    if(!task) res.status(400).json({success: false, message: `Task (${taskId} does not exist`});
+
+    // Verify if the request sender was the owner of the Task
+    const isOwner = String(task.createdBy) === String(user._id);
+    const isAdmin = String(task.createdBy) === String(user._id);
+
+    if(!isOwner && isAdmin || !isAdmin) res.status(401).json({success: false, message: 'You are not authorized to update this task'});
+
+    if(title) task.title = title;
+    if(description) task.description = description;
+    if(deadline) task.deadline = deadline;
+    if(status) task.status = status;
+
+    await task.save();
+}
+
+module.exports = { createTask, updateTask }
