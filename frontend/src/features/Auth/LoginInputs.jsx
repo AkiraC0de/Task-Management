74a391c6395c0bc9d39f5b323a9caff629f5cc6a
Akir4aa <ravenakira22@gmail.com>
2025-05-21 01:55:47 +0800

    frontend feat(LoginInputs): added onchange handler

--- frontend/src/features/Auth/LoginInputs.jsx
+++ frontend/src/features/Auth/LoginInputs.jsx
@@ -1,19 +1,38 @@
 import { Mail, Lock } from 'lucide-react'
 import InputField from '../../components/InputField'
+import { useCallback } from 'react'
 
-const LoginInputs = ({children}) => {
+const LoginInputs = ({loginData, loginDataHandler}) => {
+  //@desc On Change Handler for Input Field
+  //@req the name must be set for the input that will be handled by this
+  const onChange = useCallback((e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    loginDataHandler(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }, [])
+
   return (
     <div className="mt-10 flex flex-col">
         <InputField 
-          header='Email' 
+          header='email' 
           icon={<Mail/>} 
           type='email'
+          value={loginData.email}
+          name='email'
+          onChange={onChange}
           placeholder='Input your email'
         />
 
         <InputField 
           header='Password' 
           icon={<Lock/>} 
+          value={loginData.password}
+          onChange={onChange}
+          name='password'
           type='password'
           placeholder='Input your password'
         />
