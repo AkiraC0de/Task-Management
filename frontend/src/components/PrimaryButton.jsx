f798b269adedda7943ef482c276b06e9ddf3f031
Akir4aa <ravenakira22@gmail.com>
2025-05-21 01:53:18 +0800

    frontend feat(PrimaryButton): added button type capability

--- frontend/src/components/PrimaryButton.jsx
+++ frontend/src/components/PrimaryButton.jsx
@@ -1,12 +1,15 @@
-const PrimaryButton = ({className, onClick, children}) => {
+import { memo } from "react"
+
+const PrimaryButton = ({className, onClick,  type = 'button', children}) => {
   return (
     <button 
         className={`tezt-lg font-bold tracking-widest p-4 w-full rounded-xl bg-[#42bdec] text-white  ${className}`}
-        onClick={onClick || function(e){e.preventDefault()}}
+        onClick={onClick || function(){}}
+        type={type}
     >
         {children}
     </button>
   )
 }
 
-export default PrimaryButton
+export default memo(PrimaryButton)
