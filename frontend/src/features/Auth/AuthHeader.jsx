78adf5368719c12548387b9a8bf2a8af9f28537b
Akir4aa <ravenakira22@gmail.com>
2025-05-21 01:57:17 +0800

    frontend perf: implement memo in some loigin component

--- frontend/src/features/Auth/AuthHeader.jsx
+++ frontend/src/features/Auth/AuthHeader.jsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import loginHeaderLight from '../../assets/login_header_light.webp'
 
 const AuthHeader = ({state = 'login'}) => {
@@ -15,4 +16,4 @@ const AuthHeader = ({state = 'login'}) => {
   )
 }
 
-export default AuthHeader
+export default memo(AuthHeader)
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,16 +5,52 @@ import PrimaryButton from "../components/PrimaryButton"
 import SwitchMessage from "../features/Auth/SwitchMessage"
 
 import useAuth from "../hooks/useAuth"
+import { useCallback, useState } from "react"
+import { loginUser } from "../features/Auth/service"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const { handleUser, handleIsLogin } = useAuth();
+  const [ loginData, setLoginData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const loginDataHandler = useCallback((value) => {
+    setLoginData(value)
+  }, [loginData])
+
+  const onSubmit = useCallback(async (e) => {
+    
+    e.preventDefault();
+    try {
+      const response = await loginUser(loginData);
+      
+      if(response.request.status === 200){
+        handleUser(response.data);
+        handleIsLogin(true);
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error?.response?.data?.message)
+    }
+    
+  }, [loginData])
+
   return (
-    <form className='w-full p-6'>
+    <form 
+    onSubmit={onSubmit}
+    className='w-full p-6'>
       <AuthHeader state='login'/>
 
-      <LoginInputs/>
+      <LoginInputs loginData={loginData} loginDataHandler={loginDataHandler}/>
 
       <ForgotPassword/>
-      <PrimaryButton className='my-5'>
+      <PrimaryButton 
+        type='submit'
+        className='my-5'>
         Login
       </PrimaryButton>
       <SwitchMessage state='login'/>
