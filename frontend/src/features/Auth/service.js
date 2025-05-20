4a2166e47829df50eb17a529fd1eb6eb51458b81
Akir4aa <ravenakira22@gmail.com>
2025-05-20 21:51:48 +0800

    frontend fix(auth/service): missing await

--- frontend/src/features/Auth/service.js
+++ frontend/src/features/Auth/service.js
@@ -1,13 +1,13 @@
 import api from '../../api/server'
 
 export const loginUser = async (credentials) => {
-    return api.post('/auth/login', credentials);
+    return await api.post('/auth/login', credentials);
 }
 
 export const signupUser = async (credentials) => {
-    return api.post('/auth/signin', credentials);
+    return await api.post('/auth/signin', credentials);
 }
 
 export const logoutUser = async (credentials) => {
-    return api.get('/auth/signin');
+    return await api.get('/auth/signin');
 }
