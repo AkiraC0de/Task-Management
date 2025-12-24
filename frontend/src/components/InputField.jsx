9e4164f2d72f6d4d7733bf03766ae973c3e07380
AkiraC0de <ravenakira22@gmail.com>
2025-12-24 22:45:49 +0800

    frontend: added password visibility toggle for InputField component

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import { useState } from 'react'
+import { EyeOff, EyeIcon } from 'lucide-react';
+
+const TOGGLE_BUTTON_COLOR = "#6a7282";
+
+const TogglePasswordButton = ({showPassword, toggle}) => {
+  return(
+    <button 
+      type='button'
+      className='cursor-pointer text-gray-500'
+      onClick={toggle}
+    > 
+      {showPassword ? <EyeOff/> : <EyeIcon/>}
+    </button>
+  )
+}
 
 const InputField = ({type = 'text', header, icon, placeholder = '', onChange, value, name}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPasswordType = type == "password";
+  const inputType = isPasswordType && showPassword ? "text" : type;
+  
   return (
       <div>
-        {header && <h1 className='ml-2 font-semibold text-md mb-2'>{header}</h1>}
+        {header && <label className='ml-2 font-semibold text-md mb-2'>{header}</label>}
         <div className="flex items-center border border-gray-300 rounded-xl px-3 py-4 shadow-sm focus-within:ring-2 focus-within:ring-blue-400 gap-2">
           <span className="text-gray-500">{icon}</span>
           <input
-            type={type}
+            type={inputType}
             value={value}
             name={name}
             onChange={onChange}
             placeholder={placeholder}
             className="outline-none w-full bg-transparent text-md text-gray-900 placeholder:text-gray-500"
           />
+          { 
+            isPasswordType && 
+            <TogglePasswordButton 
+              showPassword={showPassword} 
+              toggle={() => setShowPassword(prev => !prev)}
+            /> 
+          }
         </div>
       </div>
-      
   )
 }
 
