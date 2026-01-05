8b47ebfa46127ac5e0fd6d2b93d65b61644144d1
AkiraC0de <ravenakira22@gmail.com>
2026-01-05 23:15:50 +0800

    frontend:  added custom scroll bar for html. fixed the issue in sign up page where it shows scroll bar during the first render due to the fade up animation.

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -9,7 +9,7 @@ const Signup = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 md:flex-row flex-1">
       <Hero/>
-      <div className="flex items-start justify-center">
+      <div className="flex items-start justify-center overflow-hidden">
         <div className='anim-fade-up w-full max-w-110 p-6'>
           <AuthHeader state={PAGE_STATE}/>
           <SignupForm/>
