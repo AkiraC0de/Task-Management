a10459f337cdb1a22c0684a26748fa28e809bb93
AkiraC0de <ravenakira22@gmail.com>
2026-01-06 22:07:32 +0800

    frontend: restyled the hero section of the landing page

--- frontend/src/features/Landing/Hero.jsx
+++ frontend/src/features/Landing/Hero.jsx
@@ -1,11 +1,11 @@
 const Hero = () => {
   return (
     <div className="max-w-160 flex flex-col items-center text-center">
-      <p className="bg-secondary-text/50 px-4 text-xs font-semibold py-2 w-fit text-white rounded-3xl mb-5">
+      <p className="bg-primary/60 px-3 text-xs font-semibold py-1.5 w-fit text-white rounded-3xl mb-5">
         Online Group Task Management
       </p>
-      <h1 className="text-2xl md:text-4xl text-primary font-bold">
-        Every contribution matters. Makes your teams’ hard work visible.
+      <h1 className="text-2xl md:text-4xl text-primary-text font-bold">
+        Every <span className="text-primary">contribution matters</span>. Makes your teams’ hard work visible.
       </h1>
       <p className="text-center text-sm md:text-md max-w-110 font-medium text-primary-text my-4">
         Move beyond basic to-do lists. GTask provides the structure your team needs to stay organized and the transparency required to ensure your hard work never goes unnoticed by your peers or instructors.
