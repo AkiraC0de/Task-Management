d9c7e3375cfe7deca25b09e0f3f504e67bf5391f
AkiraC0de <ravenakira22@gmail.com>
2026-01-12 00:20:12 +0800

    frontend: made the authHeader more resuable within the auth pages

--- frontend/src/features/Auth/AuthHeader.jsx
+++ frontend/src/features/Auth/AuthHeader.jsx
@@ -1,19 +1,13 @@
 import { memo } from 'react'
 
-const AuthHeader = ({state = 'login'}) => {
+const AuthHeader = ({header = "", subheader = ""}) => {
   return (
     <div className='flex flex-col justify-center items-center mb-4 gap-2'>
         <h1 className='text-3xl text-primary font-bold'>
-          { state === 'login' ?
-              'Login Account' :
-              'Sign up'
-          }
+          { header }
         </h1>
         <p className='text-center text-xs mx-2 text-primary-text'>
-          { state === 'login' ?
-              'Back to the grind. Let’s get your group to the finish line.' :
-              'Join now! No more silent partners. Track every task, together.'
-          }
+          { subheader }
         </p>
     </div>
   )
