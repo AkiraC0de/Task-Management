e45aae8c30ca066931d40ef24c4c4583c9d7e44e
AkiraC0de <ravenakira22@gmail.com>
2026-01-13 15:55:57 +0800

    backend: refactored the request reset password controller

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -189,58 +189,72 @@ const verifyEmail = async (req, res) => {
 }
 
 const verifyEmailResend = async (req, res) => {
-    const prevToken = req.token;
-    const userId = req.user._id;
-    const userEmail = req.user.email;
-    console.log(req.user)
-    if(!userId || !prevToken || !userEmail) return res.status(400).json({success: false, message: 'Missing required data'});
-;
-    // Validate if the previous token has been sent 2 minutes ago before sending a new one
-    // NEEDS TO BE UPDATED TO RATE LIMITER
-    const currentTime = new Date();
-    const timeDifference = currentTime - prevToken.createdAt; // Result is in milliseconds
-    const twoMinutesInMs = 2 * 60 * 1000;
-    const remainingSecs = Math.ceil((twoMinutesInMs - timeDifference) / 1000);
+    try {
+        const prevToken = req.token;
+        const userId = req.user._id;
+        const userEmail = req.user.email;
+        if(!userId || !prevToken || !userEmail) return res.status(400).json({success: false, message: 'Missing required data'});
+    ;
+        // Validate if the previous token has been sent 2 minutes ago before sending a new one
+        // NEEDS TO BE UPDATED TO RATE LIMITER
+        const currentTime = new Date();
+        const timeDifference = currentTime - prevToken.createdAt; // Result is in milliseconds
+        const twoMinutesInMs = 2 * 60 * 1000;
+        const remainingSecs = Math.ceil((twoMinutesInMs - timeDifference) / 1000);
 
-    if(timeDifference < twoMinutesInMs) return res.status(400).json({success: false, message: `Please wait ${remainingSecs} seconds before requesting a new code.`});
+        if(timeDifference < twoMinutesInMs) return res.status(400).json({success: false, message: `Please wait ${remainingSecs} seconds before requesting a new code.`});
 
-    // Delete the old Token
-    await prevToken.deleteOne();
+        // Delete the old Token
+        await prevToken.deleteOne();
 
-    const newVerificationCode = generateSixDigitCode().toString();
-    const newToken = crypto.randomBytes(12).toString('hex');
-    const hashedToken = crypto
-                        .createHash('sha256')
-                        .update(newToken)
-                        .digest('hex');
-    
-    await Token.create({user: userId, token: hashedToken, otp : newVerificationCode, type : 'email_verify'});
+        const newVerificationCode = generateSixDigitCode().toString();
+        const newToken = crypto.randomBytes(12).toString('hex');
+        const hashedToken = crypto
+                            .createHash('sha256')
+                            .update(newToken)
+                            .digest('hex');
+        
+        await Token.create({user: userId, token: hashedToken, otp : newVerificationCode, type : 'email_verify'});
 
-    // Send the Verification Code via email
-    const emailSubject = "New email Verification Code";
-    const emailHtml = generateResendCodeHTML(newVerificationCode);
-    await sendEmail(userEmail, emailSubject, emailHtml);
+        // Send the Verification Code via email
+        const emailSubject = "New email Verification Code";
+        const emailHtml = generateResendCodeHTML(newVerificationCode);
+        await sendEmail(userEmail, emailSubject, emailHtml);
 
-    res.status(200).json({success: true, message: `New Code has been sent to your email (${userEmail})`, token: newToken})
+        res.status(200).json({success: true, message: `New Code has been sent to your email (${userEmail})`, token: newToken});
+    } catch (error) {
+        res.status(500).json({success: false, message: `Server Error`});
+        console.log(error.message) // Should have an error handler
+    }
 }
 
 const requestResetPassword = async (req, res) => {
-    const userEmail = req.body?.email;
-    if(!userEmail) return res.status(400).json({success: false, message: 'Missing Required data: email'});
+    try {
+        const userEmail = req.body?.email;
+        if(!userEmail) return res.status(400).json({success: false, message: 'Missing Required data: email'});
 
-    const user = await User.findOne({email : userEmail});
-    if(!user) return res.status(400).json({success: false, message: 'We cannot find your email'});
+        const user = await User.findOne({email : userEmail});
+        if(!user) return res.status(200).json({success: true, message: 'If the email exist. The Reset Link has been sent'});
 
-    const accessToken = generateAccessToken(user);
-    await Token.create({user: user_id, token: accessToken});
+        const token = crypto.randomBytes(12).toString('hex');
+        const hashedToken = crypto
+                            .createHash('sha256')
+                            .update(token)
+                            .digest('hex')
 
-    const resetPasswordURL = `${process.env.FRONTEND_ORIGIN_URL}/reset-password/${accessToken}`;
+        await Token.create({user: user._id, token: hashedToken, type : 'password_reset'});
 
-    const emailSubject = "Reset Password Verification";
-    const emailHtml = generateForgotPasswordEmailHTML(resetPasswordURL, user.firstName);
-    await sendEmail(userEmail, emailSubject, emailHtml);
+        const resetPasswordURL = `${process.env.FRONTEND_ORIGIN_URL}/reset-password/${token}`;
 
-    res.status(200).json({success: true, message: `Reset password link has been sent to your email (${userEmail})`})
+        const emailSubject = "Reset Password Verification";
+        const emailHtml = generateForgotPasswordEmailHTML(resetPasswordURL, user.firstName);
+        await sendEmail(userEmail, emailSubject, emailHtml);
+
+        res.status(200).json({success: true, message: `If the email exist. The Reset Link has been sent`})
+    } catch (error) {
+        res.status(500).json({success: false, message: `Server Error`});
+        console.log(error.message) // Should have an error handler
+    }
 }
 
 // NEEDS REFACTORING
