a626a2eba3b5daf007056932e35332840e1b06eb
AkiraC0de <ravenakira22@gmail.com>
2026-01-15 11:23:23 +0800

    backend:  added a function where it verifys the tokens date creation if its two minutes ago

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -2,6 +2,15 @@ const generateSixDigitCode = () => {
   return Math.floor(100000 + Math.random() * 900000);
 };
 
+const isAuthorizedForNewToken = (prevTokenCreatedTime) => {
+  const COOLDOWN_TIME_IN_MS = 2 * 60 * 1000; // 2 minutes
+
+  const timeDifference = new Date() - prevTokenCreatedTime; // Result is in milliseconds
+
+  return timeDifference > twoMinutesInMs;
+}
+
 module.exports = { 
   generateSixDigitCode, 
+  isAuthorizedForNewToken
 };
